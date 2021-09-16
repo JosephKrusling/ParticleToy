585949a4cc48627f087ad860c9bb18e13ca45b95
Joseph Krusling <joseph@krusling.net>
2021-09-16 11:57:53 -0400

    Add helper function to convert RGB to hue

--- src/engine/types.ts
+++ src/engine/types.ts
@@ -26,15 +26,15 @@ export interface Color {
   b: number;
 }
 
-export type Interactor = (p1: Particle, p2: Particle) => V2D
+export type Interactor = (p1: Particle, p2: Particle) => V2D;
 export type BoundsEnforcer = (p: Particle) => Particle;
 
 export function add(v1: V2D, v2: V2D): V2D {
-  return {x: v1.x + v2.x, y: v1.y + v2.y};
+  return { x: v1.x + v2.x, y: v1.y + v2.y };
 }
 
 export function scale(v: V2D, scale: number): V2D {
-  return {x: v.x * scale, y: v.y * scale};
+  return { x: v.x * scale, y: v.y * scale };
 }
 
 export function dist2(v1: V2D, v2: V2D): number {
@@ -51,5 +51,31 @@ export function angleBetween(source: V2D, dest: V2D): number {
 
 export function repel(repelled: V2D, repellant: V2D, magnitude: number): V2D {
   const angle = Math.atan2(repelled.y - repellant.y, repelled.x - repellant.y);
-  return {x: Math.cos(angle) * magnitude, y: Math.sin(angle) * magnitude}
+  return { x: Math.cos(angle) * magnitude, y: Math.sin(angle) * magnitude };
+}
+
+// Return hue of an RGB color on scale 0-1
+// Converted from Java version here: https://stackoverflow.com/questions/23090019/fastest-formula-to-get-hue-from-rgb
+export function getHue(color: Color) {
+  const min = Math.min(Math.min(color.r, color.g), color.b);
+  const max = Math.max(Math.max(color.r, color.g), color.b);
+  if (min === max) {
+    return 0;
+  }
+
+  let hue = 0;
+  if (color.r === max) {
+    hue = (color.g - color.b) / (max - min);
+  } else if (color.b === max) {
+    hue = 2 + (color.b - color.r) / (max - min);
+  } else {
+    hue = 4 + (color.r - color.g) / (max - min);
+  }
+
+  hue = hue / 6;
+  if (hue < 0) {
+    hue += 6;
+  }
+
+  return hue;
 }
